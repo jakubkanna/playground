3b2af708598de45533615324473a1074f2ab9cfc
Jakub Kanna <hello.jakubkanna@gmail.com>
2024-01-11 17:59:51 +0100

    Passing Props to a Component - chall. I

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,8 +1,65 @@
 import "./App.css";
-import Greeting from "./Greeting.jsx";
 
-function App() {
-  return <Greeting />;
+function getImageUrl(imageId, size = "s") {
+  return "https://i.imgur.com/" + imageId + size + ".jpg";
 }
 
-export default App;
+function Scientist({ name, imageUrl, profession, awards, discovery }) {
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(imageUrl)}
+        alt={name}
+        width={70}
+        height={70}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Awards:{awards.length} </b>
+          {awards.join(", ")}
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovery}
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+function Gallery() {
+  return (
+    <div>
+      <h1>Notable Scientists</h1>
+      <Scientist
+        imageId="szV5sdG"
+        name="Maria Skłodowska-Curie"
+        profession="physicist and chemist"
+        discovery="polonium (chemical element)"
+        awards={[
+          "Nobel Prize in Physics",
+          "Nobel Prize in Chemistry",
+          "Davy Medal",
+          "Matteucci Medal",
+        ]}
+      />{" "}
+      <Scientist
+        imageId="YfeOqp2"
+        name="Katsuko Saruhashi"
+        profession="geochemist"
+        discovery="a method for measuring carbon dioxide in seawater"
+        awards={["Miyake Prize for geochemistry", "Tanaka Prize"]}
+      />
+    </div>
+  );
+}
+
+export default function App() {
+  return <Gallery />;
+}
