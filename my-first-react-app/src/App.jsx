31dea4a2600eeb1de0bd771219ee6d531f0ebf0a
Jakub Kanna <hello.jakubkanna@gmail.com>
2024-01-12 16:30:45 +0100

    chall.II

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,50 +1,37 @@
 import { useState } from "react";
-import { sculptureList } from "./data.js";
+export default function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
-  let hasPrev = index > 0;
-  let hasNext = index < sculptureList.length - 1;
-
-  function handleNextClick() {
-    if (hasNext) {
-      setIndex(index + 1);
-    } else {
-      setIndex(0);
-    }
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
   }
 
-  function handlePrevClick() {
-    if (hasPrev) {
-      setIndex(index - 1);
-    } else {
-      setIndex(sculptureList.length - 1);
-    }
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
   }
 
-  function handleMoreClick() {
-    setShowMore(!showMore);
+  function handleReset() {
+    setFirstName("");
+    setLastName("");
   }
 
-  let sculpture = sculptureList[index];
   return (
-    <>
-      {" "}
-      <button onClick={handlePrevClick}>Previous</button>
-      <button onClick={handleNextClick}>Next</button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? "Hide" : "Show"} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img src={sculpture.url} alt={sculpture.alt} />
-    </>
+    <form onSubmit={(e) => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        placeholder="Last name"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>
+        Hi, {firstName} {lastName}
+      </h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
   );
 }
