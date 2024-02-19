1633e89063f3abce145f7773b1013e6434cf7961
Jakub Kanna <hello.jakubkanna@gmail.com>
2024-02-19 13:03:14 +0100

    add to file

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,8 +1,8 @@
 const fs = require("node:fs/promises");
 async function example() {
   try {
-    const content = "Some async content!";
-    await fs.writeFile("./test.txt", content);
+    const content = "Some content!";
+    await fs.appendFile("./test.txt", content);
   } catch (err) {
     console.log(err);
   }
