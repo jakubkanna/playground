d3927f05fed14f96dc1aa32579d11b6b29cd5090
Jakub Kanna <hello.jakubkanna@gmail.com>
2024-02-19 12:59:39 +0100

    Writing a file

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,20 +1,9 @@
-#!/usr/bin/env node
-
-const http = require("node:http");
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const array = [
-  process.env.USER_ID, // "239482"
-  process.env.USER_KEY, // "foobar"
-  process.env.PORT, //3000
-];
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end(array.toString());
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const fs = require("node:fs");
+const content = "Some content!";
+fs.writeFile("./test.txt", content, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    // file written successfully
+  }
 });
